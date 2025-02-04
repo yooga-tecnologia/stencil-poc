f79eda7c4ae7a75732183b197bb3137602f56109
Ana Có <ana.cbe14@gmail.com>
2025-02-04 11:12:34 -0300

    chore: add sass plugin

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'study',
@@ -24,4 +25,7 @@ export const config: Config = {
   testing: {
     browserHeadless: "shell",
   },
+  plugins: [
+    sass()
+  ]
 };
