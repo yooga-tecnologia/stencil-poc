e1d554d1807a9e6d51252354840ab892e5822963
biancardii <biancardii>
2025-02-10 13:53:59 -0300

    feat: adjusted styling in InputGroup and Update version package

--- src/components/yoo-icon/yoo-icon.tsx
+++ src/components/yoo-icon/yoo-icon.tsx
@@ -69,7 +69,7 @@ export class IconComponent {
     this.iconSize = this.bgSize ? `${baseSize - sizeMap.small}px` : `${baseSize}px`;
   }
 
-  getTransform(iconName: string): string {
+  getTransform(iconName: string): string { // TODO: Melhorar type em vez de string para ['up', 'down', 'right', 'left']
     if (iconName.includes('-up')) {
       return 'rotate(0)'; // Nenhuma rotação necessária para cima
     } else if (iconName.includes('-down')) {
